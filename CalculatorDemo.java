942a244d329a6d10bd50b4bfc7c846d0bc8e9709
danex11 <danex11@op.pl>
2022-08-01 08:28:53 +0200

    functional group 70 "user alarms"

--- CalculatorDemo.java
+++ CalculatorDemo.java
@@ -2,77 +2,70 @@ import java.util.Scanner;
 
 class CalculateAlarm {
     String alarm;
-    int dbbyte;
-    int dbbit;
+    String dbaddress;
  
      
  public CalculateAlarm(String alarm){
      this.alarm = alarm;
-     dbbyte = 0; //= Integer.parseInt(alarm);
+    dbaddress = "nn";
 
     String chars12 = alarm.substring(0, 2);
-    String chars34 = alarm.substring(2, 4);
-    String chars567 = alarm.substring(4, 7);
- 
+    String chars34s = alarm.substring(2, 4);
+    int digs34 = Integer.parseInt(chars34s);
+    String chars56s = alarm.substring(4, 6);
+    int digs56 = Integer.parseInt(chars56s);
+
+
      if (new String(chars12).equals("70"))
      {
-         dbbyte = 100;
+         dbaddress = group70(digs34, digs56);
      }
-     else{
-        dbbyte=999;
+     else if (new String(chars12).equals("60"))
+     {
+        dbaddress="TODO group 60";
      }
+    else if (new String(chars12).equals("50"))
+    {
+        dbaddress="TODO group 50";
+    }   
+    else { dbaddress="not found";}
  }
  
 
+    public String getDbAddress(){
+     return dbaddress;
+    }
  
- public int getDbByte(){
-    return dbbyte;
- }
- 
- 
- 
-public void group70(String chars34, String chars567)
-{
+    public String group70(int digs34loc, int digs56loc)
+    {
+        int byte70 = 180 + (8 * digs34loc);
+        byte70 += digs56loc/8;        //truncate (to int) (i.e. drop everything after the decimal dot)
+        int bit70 = digs56loc%8;
+        String bb = String.format("byte %3d bit %2d", byte70, bit70);
+        return bb;
+    }
 
+    public void group60(int digs34, int digs567)
+    {
+
+    }
+
+    public void group50(int digs34, int digs567)
+    {
+
+    }
 }
-
-public void group60(String chars34, String chars567)
-{
-
-}
-
-public void group50(String chars34, String chars567)
-{
-
-}
-
 
 public class CalculatorDemo{
 
-    public void main(String[] args){
+    public static void main(String[] args){
         System.out.println("Write six digit alarm no.: ");
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
         CalculateAlarm calculateInstance = new CalculateAlarm(input);
-        System.out.println("For alarm " + input + " adress in DB2 is " + calculateInstance.getDbByte());
+        System.out.println("For alarm " + input + " adress in DB2 is " + calculateInstance.getDbAddress());
         in.close();
   
-
-
-
     }
-
-
-
-    // public int calculate(String alarmNo){
-    //     String alarmNo;
-    //     this.alarmNo = alarmNo;
-    //     int byte;
-
-
-
-    //     return byte
-    // }
-
 }
-}
+
